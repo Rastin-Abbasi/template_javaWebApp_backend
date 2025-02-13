b5073779e8d33f6a8a807db528c10ed5edee696b
Rastin-Abbasi <Rastin.Abbasi@gmail.com>
2025-02-13 13:33:51 -0500

    Snake Game: Players control the direction of the snake, collect food, and the snake grows in length when it eats food.

--- src/main/java/mapper/SnakeMapper.java
+++ src/main/java/mapper/SnakeMapper.java
@@ -3,4 +3,8 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface SnakeMapper {
     // Define SQL queries to store and retrieve game data
+    
+    void saveGameData(GameData gameData);
+    
+    GameData getGameDataById(int id);
 }
